c6ec10527bb8fdbdd6e70bbea20b702b58b1d534
Marcono1234 <Marcono1234@users.noreply.github.com>
2021-06-09 03:21:49 +0200

    Fix update-accuracy-reports.sh not detecting rejected push

--- .github/workflow-scripts/update-accuracy-reports.sh
+++ .github/workflow-scripts/update-accuracy-reports.sh
@@ -12,11 +12,16 @@ if [[ $(git status --porcelain) ]]; then
   git config --global user.name "GitHub Actions"
   git commit -m "Update accuracy reports" -m "Accuracy reports for $commitSha"
   
-  # TODO: Not tested
-  if [[ $(git push --porcelain | grep -q "! [rejected]") ]]; then
-    echo "Failed pushing accuracy reports changes; new commit has probably been pushed in the meantime"
-  else
+  if output=$(git push --porcelain); then
     echo "Pushed accuracy reports changes"
+  else
+    if [[ $output == *"[rejected] (fetch first)"* ]]; then 
+      echo "Failed pushing accuracy reports changes; new commit has probably been pushed in the meantime"
+    else
+      echo "Pushing changes failed"
+      echo "$output"
+      exit 1
+    fi
   fi
 else
   echo "No accuracy reports changes"
