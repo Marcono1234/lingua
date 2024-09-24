bd0ed6923af20ea01764da223a01992e86cc2a4e
Marcono1234 <Marcono1234@users.noreply.github.com>
2024-09-24 03:02:11 +0200

    Configure auto-provisioning of JDK toolchain for Gradle

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -5,3 +5,8 @@ dependencyResolutionManagement {
         }
     }
 }
+
+// Automatically download JDK toolchain if necessary, see https://docs.gradle.org/8.10.1/userguide/toolchains.html#sec:provisioning
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
